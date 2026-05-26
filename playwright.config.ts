520f6d56176b90ea5426e2c7497f140aaafd2c16
pixbs <hey@pixbs.com>
2026-05-26 08:19:10 +0200

    chore: 🛠️ Update Playwright configuration and add CI workflow for testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,10 +21,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
   projects: [
@@ -34,8 +31,8 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'bun run dev',
-    reuseExistingServer: true,
+    command: process.env.CI ? 'bun start' : 'bun run dev',
+    reuseExistingServer: !process.env.CI,
     url: 'http://localhost:3000',
   },
 })
